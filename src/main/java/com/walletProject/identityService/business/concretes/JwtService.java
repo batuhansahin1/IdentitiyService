e7bfa7ea74ca6e09148af39f1292097c2df9d41f
batuhansahin1 <batuhansahin466@gmail.com>
2026-05-11 21:05:49 +0300

    rabbitmq setup

--- src/main/java/com/walletProject/identityService/business/concretes/JwtService.java
+++ src/main/java/com/walletProject/identityService/business/concretes/JwtService.java
@@ -10,6 +10,7 @@ import java.util.function.Function;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,7 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 
+	@Value("${jwt.secret}")
 private String secretKey;
 	
 	public JwtService() {
